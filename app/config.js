30a1de9e6bf9749191b0b11d908b826904fe3181
Pete Blakemore <pete.blakemore@digital.education.gov.uk>
2018-03-09 14:50:56 +0000

    Changed service name
    Changed from "EPAO Certification" to "Record end point assessment outcome"

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'EPAO Certification',
+  serviceName: 'Record end point assessment outcome',
 
   // Default port that prototype runs on
   port: '3000',
